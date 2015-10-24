0fc9ea97151a86dcc41566fd6eb5e3827f3d3ae2
Kris Brandow <kris.brandow@gmail.com>
2015-10-24 18:45:53 -0400

    Removing old code.

--- parsing.c
+++ parsing.c
@@ -260,54 +260,6 @@ lval* lval_read(mpc_ast_t* t) {
     return x;
 }
 
-/* Use operator string to see which operation to perform */
-/* lval eval_op(lval x, char* op, lval y) { */
-/*  */
-/*     #<{(| If either value is an error return it |)}># */
-/*     if (x.type == LVAL_ERR) { return x; } */
-/*     if (y.type == LVAL_ERR) { return y; } */
-/*  */
-/*     #<{(| Otherwise do maths on the number values |)}># */
-/*     if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); } */
-/*     if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); } */
-/*     if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); } */
-/*     if (strcmp(op, "/") == 0) { */
-/*         #<{(| If second operand is zero return error |)}># */
-/*         return y.num == 0 */
-/*             ? lval_err(LERR_DIV_ZERO) */
-/*             : lval_num(x.num / y.num); */
-/*     } */
-/*     if (strcmp(op, "%") == 0) { return lval_num(fmod(x.num, y.num)); } */
-/*     if (strcmp(op, "^") == 0) { return lval_num(powl(x.num, y.num)); } */
-/*     return lval_err(LERR_BAD_OP); */
-/* } */
-/*  */
-/* lval eval(mpc_ast_t* t) { */
-/*  */
-/*     #<{(| If tagged as number return it directly. |)}># */
-/*     if (strstr(t->tag, "number")) { */
-/*         #<{(| Check if there is some error in conversion |)}># */
-/*         errno = 0; */
-/*         double x = strtod(t->contents, NULL); */
-/*         return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM); */
-/*     } */
-/*  */
-/*     #<{(| The operator is always second child. |)}># */
-/*     char* op = t->children[1]->contents; */
-/*  */
-/*     #<{(| We store the third child in `x` |)}># */
-/*     lval x = eval(t->children[2]); */
-/*  */
-/*     #<{(| Iterate the remaining children and combining. |)}># */
-/*     int i = 3; */
-/*     while(strstr(t->children[i]->tag, "expr")) { */
-/*         x = eval_op(x, op, eval(t->children[i])); */
-/*         i++; */
-/*     } */
-/*  */
-/*     return x; */
-/* } */
-
 int main(int argc, char *argv[])
 {
     /* Create some Parser */
